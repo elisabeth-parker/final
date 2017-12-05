3960e47e9f2f0d44fd860ad1bb3ec937111c4b4a
elisabeth-parker <31249770+elisabeth-parker@users.noreply.github.com>
2017-12-05 09:36:55 -0500

    map files
    used in iframe on icons page

---
+++ map/js/data.js
@@ -0,0 +1,414 @@
+eqfeed_callback(
+  {"type":"FeatureCollection",
+  "features":[
+    {"type":"Feature",
+      "properties":
+       {
+         "place": "AK",
+         "count": 9,
+       },
+        "geometry":
+          {"type":"Point",
+          "coordinates":[-152.2307,64.061]},
+        "id":"Arkansas"
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "AL",
+           "count": 258,
+         },
+          "geometry":
+           {"type":"Point","coordinates":[-86.829,32.7784]
+         }
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "AR",
+           "count": 222,
+         },
+         "geometry":
+          {"type":"Point","coordinates":[-92.4429,34.8938]
+         }
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "AZ",
+           "count": 242,
+         }, "geometry":
+         {"type":"Point","coordinates":[-111.6582,34.274]
+       }
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "CA",
+           "count": 2044,
+         }, "geometry": {"type":"Point","coordinates":[-119.4505,37.1992]
+           },
+        },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "CO",
+           "count": 1364,
+         }, "geometry": {"type":"Point","coordinates":[-105.5495,38.9979]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "CT",
+           "count": 964,
+         }, "geometry": {"type":"Point","coordinates":[-72.7272,41.622]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "DC",
+           "count": 421,
+         }, "geometry": {"type":"Point","coordinates":[-77.0147,38.9101]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "DE",
+           "count": 99,
+         }, "geometry": {"type":"Point","coordinates":[-75.5051,38.9895]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "FL",
+           "count": 691,
+         }, "geometry": {"type":"Point","coordinates":[-82.4426,28.6282]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "GA",
+           "count": 991,
+         }, "geometry": {"type":"Point","coordinates":[-83.4418,32.6405]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "HI",
+           "count": 1,
+         }, "geometry": {"type":"Point","coordinates":[-156.372,20.2951]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "IA",
+           "count": 386,
+         }, "geometry": {"type":"Point","coordinates":[-93.4959,42.0753]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "ID",
+           "count": 330,
+         }, "geometry": {"type":"Point","coordinates":[-114.6086,44.3538]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "IL",
+           "count": 1441,
+         }, "geometry": {"type":"Point","coordinates":[-89.1965,40.0411]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "IN",
+           "count": 756,
+         }, "geometry": {"type":"Point","coordinates":[-86.2818,39.8938]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "KS",
+           "count": 91,
+         }, "geometry": {"type":"Point","coordinates":[-98.3801,38.4949]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "KY",
+           "count": 179,
+         }, "geometry": {"type":"Point","coordinates":[-85.3021,37.536]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "LA",
+           "count": 122,
+         }, "geometry": {"type":"Point","coordinates":[-91.9952,31.0689]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "MA",
+           "count": 2682,
+         }, "geometry": {"type":"Point","coordinates":[-71.8083,42.2596]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "MD",
+           "count": 613,
+         }, "geometry": {"type":"Point","coordinates":[-76.7909,39.055]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "ME",
+           "count": 831,
+         }, "geometry": {"type":"Point","coordinates":[-69.2438,45.3702]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "MI",
+           "count": 533,
+         }, "geometry": {"type":"Point","coordinates":[-85.4114,44.3461]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "MN",
+           "count": 2726,
+         }, "geometry": {"type":"Point","coordinates":[-94.3046,46.281]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "MO",
+           "count": 696,
+         }, "geometry": {"type":"Point","coordinates":[-92.4571,38.3568]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "MS",
+           "count": 87,
+         }, "geometry": {"type":"Point","coordinates":[-89.668,32.7351]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "MT",
+           "count": 240,
+         }, "geometry": {"type":"Point","coordinates":[-109.645,47.0527]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "NC",
+           "count": 2183,
+         }, "geometry": {"type":"Point","coordinates":[-79.3856,35.5579]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "NE",
+           "count": 65,
+         }, "geometry": {"type":"Point","coordinates":[-99.7968,41.5392]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "NH",
+           "count": 234,
+         }, "geometry": {"type":"Point","coordinates":[-71.5818,43.6805]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "NJ",
+           "count": 704,
+         }, "geometry": {"type":"Point","coordinates":[-74.6733,40.1907]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "NM",
+           "count": 32,
+         }, "geometry": {"type":"Point","coordinates":[-106.1128,34.4066]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "NV",
+           "count": 59,
+         }, "geometry": {"type":"Point","coordinates":[-116.6268,39.3306]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "NY",
+           "count": 2821,
+         }, "geometry": {"type":"Point","coordinates":[-75.5262,42.9543]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "OH",
+           "count": 1359,
+         }, "geometry": {"type":"Point","coordinates":[-82.7938,40.2863]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "OK",
+           "count": 97,
+         }, "geometry": {"type":"Point","coordinates":[-97.4943,35.591]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "OR",
+           "count": 489,
+         }, "geometry": {"type":"Point","coordinates":[-120.5617,43.9336]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "PA",
+           "count": 1816,
+         }, "geometry": {"type":"Point","coordinates":[-77.7985,40.8786]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "RI",
+           "count": 194,
+         }, "geometry": {"type":"Point","coordinates":[-71.5562,41.6762]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "SC",
+           "count": 234,
+         }, "geometry": {"type":"Point","coordinates":[-80.8957,33.9169]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "SD",
+           "count": 23,
+         }, "geometry": {"type":"Point","coordinates":[-100.2282,44.4446]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "TN",
+           "count": 483,
+         }, "geometry": {"type":"Point","coordinates":[-86.3504,35.8605]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "TX",
+           "count": 1453,
+         }, "geometry": {"type":"Point","coordinates":[-99.3287,31.4789]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "UT",
+           "count": 663,
+         }, "geometry": {"type":"Point","coordinates":[-111.6685,39.3057]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "VA",
+           "count": 906,
+         }, "geometry": {"type":"Point","coordinates":[-78.8531,37.5229]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "VT",
+           "count": 190,
+         }, "geometry": {"type":"Point","coordinates":[-72.6663,44.0688]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "WA",
+           "count": 759,
+         }, "geometry": {"type":"Point","coordinates":[-120.4505,47.3822]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "WI",
+           "count": 972,
+         }, "geometry": {"type":"Point","coordinates":[-89.9941,44.6243]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "WV",
+           "count": 44,
+         }, "geometry": {"type":"Point","coordinates":[-80.623,38.6409]
+       },
+       },
+       {"type":"Feature",
+         "properties":
+          {
+           "place": "WY",
+           "count": 1,
+         }, "geometry": {"type":"Point","coordinates":[-107.5527,42.9959]
+       },
+       },
+      ]
+    }
+  )
---
+++ map/js/testdata.js
@@ -0,0 +1,38 @@
+eqfeed_callback(
+  {"type":"FeatureCollection",
+  "features":[
+    {"type":"Feature",
+      "properties":
+        {"count":9,
+        "place":"48km SSE of Pondaguita, Philippines",
+        },
+      "geometry":
+        {"type":"Point",
+        "coordinates":[-152.2307,64.061]},
+      "id":"usc000csx3"},
+    {"type":"Feature",
+      "properties":
+        {"count":5.7,
+        "place":"35km ESE of Ndoi Island, Fiji",
+        "time":1348175020,
+        "tz":-720,
+        "url":"http://earthquake.usgs.gov/earthquakes/eventpage/usc000csw5",
+        "felt":0,
+        "cdi":1,
+        "mmi":2,
+        "alert":"green",
+        "status":"REVIEWED",
+        "tsunami":null,
+        "sig":"500",
+        "net":"us",
+        "code":"c000csw5",
+        "ids":",usc000csw5,",
+        "sources":",us,",
+        "types":",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"},
+      "geometry":
+        {"type":"Point",
+        "coordinates":[-86.829, 32.7784]},
+      "id":"usc000csw5"}
+    ]
+  }
+)
